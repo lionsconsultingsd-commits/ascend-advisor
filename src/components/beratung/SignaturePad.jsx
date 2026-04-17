42067b8cb355dfdd92085dbd545262aca210f0e0
Simon <lionsconsulting.sd@gmail.com>
2026-04-17 12:06:10 +0000

    File changes

--- src/components/beratung/SignaturePad.jsx
+++ src/components/beratung/SignaturePad.jsx
@@ -23,13 +23,18 @@ export default function SignaturePad({ onSigned, existingUrl }) {
 
   const getPos = (e, canvas) => {
     const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
     if (e.touches) {
       return {
-        x: e.touches[0].clientX - rect.left,
-        y: e.touches[0].clientY - rect.top,
+        x: (e.touches[0].clientX - rect.left) * scaleX,
+        y: (e.touches[0].clientY - rect.top) * scaleY,
       };
     }
-    return { x: e.clientX - rect.left, y: e.clientY - rect.top };
+    return {
+      x: (e.clientX - rect.left) * scaleX,
+      y: (e.clientY - rect.top) * scaleY,
+    };
   };
 
   const handleStart = (e) => {
